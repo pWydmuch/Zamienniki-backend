33b8b994c73fc0ece59c76e00a41b213681565cd
Patryk Wydmuch <pvydmuch@gmail.com>
2020-01-29 09:29:26 +0100

    Start writing tests

--- src/test/java/wydmuch/patryk/zamienniki/services/impl/KursServiceImplTest.java
+++ src/test/java/wydmuch/patryk/zamienniki/services/impl/KursServiceImplTest.java
@@ -1,36 +1,17 @@
 package wydmuch.patryk.zamienniki.services.impl;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
-import wydmuch.patryk.zamienniki.dto.KursDto;
-import wydmuch.patryk.zamienniki.entities.Kurs;
-import wydmuch.patryk.zamienniki.entities.PlanStudiow;
-import wydmuch.patryk.zamienniki.entities.Przedmiot;
-import wydmuch.patryk.zamienniki.services.PlanStudiowService;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
 class KursServiceImplTest {
 
-
-    @BeforeEach
-    void setUp() {
+    @Test
+    void filterKursy() {
     }
 
     @Test
-    void getKursySearch() {
-
-    }
-
-    @Test
-    void checkIfContains() {
-
-    }
-
-    @Test
-    void getKierunki() {
+    void checkIfKursContainsFormaZajec() {
+//        Kurs = new
     }
 }
